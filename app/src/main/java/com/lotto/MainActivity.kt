7e979031d9123ff3aec6d556bc0f61b5553ac5a4
김민규 <mcgee@wemakeprice.com>
2022-01-07 23:23:57 +0900

    Use Log

--- app/src/main/java/com/lotto/MainActivity.kt
+++ app/src/main/java/com/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.lotto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        Log.d("BasicSyntax-debug", "로그 method = Log.d") // debug
+        Log.v("BasicSyntax-verbose", "로그 method = Log.v") // verbose
+        Log.i("BasicSyntax-information", "로그 method = Log.i") // information
+        Log.w("BasicSyntax-warning", "로그 method = Log.w") // warning
+        Log.e("BasicSyntax-error", "로그 method = Log.e") // error
+
 
         setContentView(binding.root)
         binding.btnSay.setOnClickListener {
